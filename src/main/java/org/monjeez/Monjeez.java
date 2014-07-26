205b1aae9d61f7a590440bcfb1fa7ea9202d8c68
lstolowski <lstolowski@objectivity.co.uk>
2014-07-26 16:25:59 +0200

    Initial structure 2

---
+++ src/main/java/org/monjeez/Monjeez.java
@@ -0,0 +1,20 @@
+package org.monjeez;
+
+import com.mongodb.Mongo;
+
+/**
+ * Monjeez runner
+ * @author lstolowski
+ * @since 26/07/2014
+ */
+public class Monjeez {
+
+  private Mongo mongo;
+  private String dbName;
+
+  private String userName;
+  private String passWord;
+  
+  
+  
+}
