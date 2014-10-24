b3c6bb5c28b13600550cc20af41352f9992ee2cd
lstolowski <stolowski@wp.pl>
2014-10-24 13:16:42 +0200

    developing index creation for dbchangelog collection

--- src/main/java/com/github/mongobee/dao/ChangeEntryDao.java
+++ src/main/java/com/github/mongobee/dao/ChangeEntryDao.java
@@ -4,9 +4,13 @@ import com.github.mongobee.changeset.ChangeEntry;
 import com.github.mongobee.exception.MongobeeConfigurationException;
 import com.github.mongobee.exception.MongobeeConnectionException;
 import com.mongodb.*;
+import org.omg.PortableInterceptor.LOCATION_FORWARD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.*;
 
 import java.net.UnknownHostException;
+import java.util.List;
 
 import static com.github.mongobee.changeset.ChangeEntry.CHANGELOG_COLLECTION;
 import static org.springframework.util.StringUtils.hasText;
@@ -16,20 +20,26 @@ import static org.springframework.util.StringUtils.hasText;
  * @since 27/07/2014
  */
 public class ChangeEntryDao {
+  private static final Logger logger = LoggerFactory.getLogger("Mongobee dao");
 
   private DB db;
-  
+
+  public DB getDb() {
+    return db;
+  }
+
   public DB connectMongoDb(Mongo mongo, String dbName) throws UnknownHostException {
     if (!hasText(dbName)){
       throw new MongobeeConfigurationException("DB name is not set. Should be defined in MongoDB URI or via setter");
     } else {
       db = mongo.getDB(dbName);
+      // ensureChangelogCollectionIndexes(db.getCollection(CHANGELOG_COLLECTION)); // TODO Issue#14
       return db;
     }
   }
 
   public DB connectMongoDb(MongoClientURI mongoClientURI, String dbName) throws UnknownHostException {
-    MongoClient mongoClient = new MongoClient(mongoClientURI);
+    Mongo mongoClient = new MongoClient(mongoClientURI);
     String database = (!hasText(dbName)) ? mongoClientURI.getDatabase() : dbName;
     return this.connectMongoDb(mongoClient, database);
   }
@@ -57,7 +67,32 @@ public class ChangeEntryDao {
     }
   }
 
-  public DB getDb() {
-    return db;
+  // TODO Issue#14: add changeId_1_author_1 + unique: true
+  private void ensureChangelogCollectionIndexes(DBCollection collection) {
+
+    List<DBObject> indexInfo = collection.getIndexInfo();
+    for (com.mongodb.DBObject ind : indexInfo){
+      Object key = ind.get("key");
+      if (key instanceof BasicDBObject){
+
+        // FIXME refactor: 1. search for it, if no: create, else check if unique if no: delete & add unique,
+
+        BasicDBObject indexKey = (BasicDBObject) key;
+        if (indexKey.get("changeId") instanceof Integer &&  ((Integer)indexKey.get("changeId")) == 1 &&
+            indexKey.get("author") instanceof Integer &&  ((Integer)indexKey.get("author")) == 1){
+
+          //indexes exist
+          logger.debug("Indexes in collection " + CHANGELOG_COLLECTION + " are correct");
+        } else {
+          collection.createIndex(new BasicDBObject().append("changeId", 1).append("author", 1));
+
+          // indexes created
+          logger.debug("Indexes in collection " + CHANGELOG_COLLECTION + " was created");
+        }
+
+      }
+    }
+
   }
+
 }
