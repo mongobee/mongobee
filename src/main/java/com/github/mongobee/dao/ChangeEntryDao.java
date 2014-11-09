07728a05c415c7aae1e2bdd93d775f6d01ce23df
lstolowski <stolowski@wp.pl>
2014-11-09 21:34:29 +0100

    issue14: logic for ensuring index is done, there is a bug with comparing indexes :(

--- src/main/java/com/github/mongobee/dao/ChangeEntryDao.java
+++ src/main/java/com/github/mongobee/dao/ChangeEntryDao.java
@@ -4,13 +4,10 @@ import com.github.mongobee.changeset.ChangeEntry;
 import com.github.mongobee.exception.MongobeeConfigurationException;
 import com.github.mongobee.exception.MongobeeConnectionException;
 import com.mongodb.*;
-import org.omg.PortableInterceptor.LOCATION_FORWARD;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.*;
 
 import java.net.UnknownHostException;
-import java.util.List;
 
 import static com.github.mongobee.changeset.ChangeEntry.CHANGELOG_COLLECTION;
 import static org.springframework.util.StringUtils.hasText;
@@ -33,7 +30,7 @@ public class ChangeEntryDao {
       throw new MongobeeConfigurationException("DB name is not set. Should be defined in MongoDB URI or via setter");
     } else {
       db = mongo.getDB(dbName);
-       ensureChangelogCollectionIndexes(db.getCollection(CHANGELOG_COLLECTION)); // TODO Issue#14
+       ensureChangeLogCollectionIndex(db.getCollection(CHANGELOG_COLLECTION)); // TODO Issue#14
       return db;
     }
   }
@@ -67,51 +64,44 @@ public class ChangeEntryDao {
     }
   }
 
-  // TODO Issue#14: add changeId_1_author_1 + unique: true
-  private void ensureChangelogCollectionIndexes(DBCollection collection) {
-
-    DBObject index = findRequiredIndex(collection);
-    if (index != null && !isUnique(index)){
-      // todo remove index,
-
-
-      // todo create new one
-      //collection.createIndex(new BasicDBObject().append("changeId", 1).append("author", 1), new BasicDBObject("unique", true));
-
-    } else if (index == null) {
-      // todo create index
-      // collection.createIndex(new BasicDBObject().append("changeId", 1).append("author", 1), new BasicDBObject("unique", true));
-
-      // indexes created
-      logger.debug("Indexes in collection " + CHANGELOG_COLLECTION + " was created");
+  private void ensureChangeLogCollectionIndex(DBCollection collection) {
+    DBObject index = findRequiredChangeAuthorIndex(collection);
+    if (index == null){
+      createRequiredUniqueIndex(collection);
+      logger.debug("Index in collection " + CHANGELOG_COLLECTION + " was created");
+    } else if (!isUnique(index)) {
+      collection.dropIndex((BasicDBObject) index.get("name"));
+      createRequiredUniqueIndex(collection);
+      logger.debug("Index in collection " + CHANGELOG_COLLECTION + " was recreated");
     }
-
 
   }
 
+  private void createRequiredUniqueIndex(DBCollection collection) {
+    collection.createIndex(new BasicDBObject()
+                              .append("changeId", 1)
+                              .append("author", 1),
+                            new BasicDBObject()
+                              .append("unique", true));
+  }
+
+  private DBObject findRequiredChangeAuthorIndex (DBCollection collection) {
+    for (DBObject index : collection.getIndexInfo()){
+      Object key = index.get("key");
+      if (key.equals(new BasicDBObject().append("changeId",1).append("author",1))){ // TODO: wrong checking :(
+        return index;
+      }
+    }
+    return null;
+  }
+
   private boolean isUnique(DBObject index) {
-    if (index.get("unique") != null && index.get("unique") instanceof Boolean){
-      return (Boolean) index.get("unique");
+    Object unique = index.get("unique");
+    if (unique != null && unique instanceof Boolean){
+      return (Boolean) unique;
     } else {
       return false;
     }
   }
-
-  private DBObject findRequiredIndex(DBCollection collection) {
-    List<DBObject> indices = collection.getIndexInfo();
-    for (com.mongodb.DBObject index : indices){
-      Object key = index.get("key");
-      if (key.equals(new BasicDBObject().append("changeId",1).append("author",1))){
-        // todo here it is a problem: changeId_1_author_1 equals to author_1_changeId_1 !!!
-        // solution: return collection and remove all not unique
-        // solution2: check here if unique if not remove   << better?
-        return index;
-      }
-    }
-    return null;  // not found
-  }
-
-  // todo refactor index checking and creating to separate util class
-
 
 }
